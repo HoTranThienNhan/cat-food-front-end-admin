c9aca742e18d1c4290b447cee5ee4255c920fb43
HoTranThienNhan <nhanhotranthien@gmail.com>
2023-11-20 20:35:14 +0700

    Modify V1

--- src/views/ProductManagementPage.vue
+++ src/views/ProductManagementPage.vue
@@ -339,7 +339,7 @@ const showModalAddProduct = () => {
 
                     <a-form-item label="Mô Tả Sản Phẩm" name="description"
                         :rules="[{ required: true, message: 'Mô tả không được để trống.' }]">
-                        <a-textarea v-model:value="addedProduct.description" placeholder="Basic usage" :rows="3" />
+                        <a-textarea v-model:value="addedProduct.description" :rows="3" />
                     </a-form-item>
 
                     <div style="margin-bottom: 10px;">
@@ -418,7 +418,7 @@ const showModalAddProduct = () => {
 
             <a-form-item label="Mô Tả Sản Phẩm" name="description"
                 :rules="[{ required: true, message: 'Mô tả không được để trống.' }]">
-                <a-textarea v-model:value="updatedProduct.description" placeholder="Basic usage" :rows="3" />
+                <a-textarea v-model:value="updatedProduct.description" :rows="3" />
             </a-form-item>
 
             <div style="margin-bottom: 10px;">
